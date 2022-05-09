716ef2334d11c3787a1ee70b8ec6fe7a8bfd60f6
tyler <tyler88128949@gmail.com>
2022-05-09 13:06:27 +0800

    finish insertion sort

---
+++ Searching/insertion_sort/sol1.py
@@ -0,0 +1,14 @@
+# Best: O(n) time | O(1) space
+# Average: O(n^2) time | O(1) space
+# Worst: O(n^2) time | O(1) space
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(array, j, j - 1)
+            j = j - 1
+    return array
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
